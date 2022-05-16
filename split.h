2a97ab33bb19d95761d5f900b5bd9ac6154a78ea
tomgo <tomgosling57@gmail.com>
2022-05-16 22:04:10 +1000

    Improved split function to take a string separator instead of a character

--- split.h
+++ split.h
@@ -1,28 +1,34 @@
-//
-// Created by xander on 5/15/22.
-//
-
 #ifndef _ASSESSMENT2_SPLIT_H
 #define _ASSESSMENT2_SPLIT_H
 #pragma once
 #include <vector>
 #include <string>
-    static void split( std::string str, char separator, std::vector<std::string > &vec, bool skipEmptyStrings = true)
-    {
-        size_t s = 0;
-        for (size_t i = 0; i < str.length(); i++)
-        {
-            if (str[i] != separator)
-                continue;
-            std::string xstr = str.substr(s, i -s);
-            if (xstr.length() || !skipEmptyStrings)
-                vec.push_back( xstr );
 
-            s = i+1;
+using std::string;
+using std::vector;
+
+static void split( std::string str, std::string separator, std::vector<std::string > &vec, bool skipEmptyStrings)
+    {
+    // fist index in the string that hasn't been check so far
+    size_t substr_index = 0;
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        if (str.substr(i, separator.length()) == separator)
+        {
+            string subStr = str.substr(substr_index, i-substr_index);
+            if (subStr.length() || !skipEmptyStrings)
+            {
+                vec.push_back(subStr);
+            }
+            substr_index = i+separator.length();
         }
-        std::string xstr = str.substr(s);
-        if (xstr.length())
-            vec.push_back(  xstr );
+
+    }
+    string subStr = str.substr(substr_index, str.length()-substr_index);
+            if (subStr.length())
+            {
+                vec.push_back(subStr);
+            }
     }
 
 
